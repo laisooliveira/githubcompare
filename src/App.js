ed5fa5095a079fa938fec5bbeeccc926e62c4584
Lais Aguiar de Oliveira <lais.aguiar.info@gmail.com>
2018-05-29 23:33:20 -0300

    Github Compare

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Main from './pages/Main';
+import './styles/global';
+
+const App = () => (
+  <div className="App">
+    <Main />
+  </div>
+);
+
+export default App;
